bc7808128324ad37fa21a7eff911970510a01cbd
Jodi Giordano <jodi@ephemeregames.com>
2013-09-15 13:05:48 -0400

    Fixed indentation issues.

--- playback.js
+++ playback.js
@@ -1,356 +1,334 @@
-/**
- * author: Guillaume Malette <gmalette@gmail.com> @gmalette
- * author: Christian Blais <christ.blais@gmail.com> @christianblais
- * author: Jodi Giordano <giordano.jodi@gmail.com> @jodi
- * website: thirdside.ca
- * date: 12/04/2011
- */
-
 TS.Playback = Class.create(TS, {
-	initialize: function($super, containers, map_url, history_url)
-	{
-		this.containers = containers;
-		this.map_url = map_url;
-		this.history_url = history_url;
+  initialize: function ($super, containers, map_url, history_url) {
+    this.containers = containers;
+    this.map_url = map_url;
+    this.history_url = history_url;
 
-		// info table row name template
-		this.playerInfoName = 'info_player_#{id}';
+    // info table row name template
+    this.playerInfoName = 'info_player_#{id}';
 
-		// other shit
-		this.playerList = $(containers.player_list);
-		this.progressBar = $(containers.progress_bar);
-		this.progressTurns = $(containers.turns);
-		this.controls = $(containers.controls);
-	},
+    // other shit
+    this.playerList = $(containers.player_list);
+    this.progressBar = $(containers.progress_bar);
+    this.progressTurns = $(containers.turns);
+    this.controls = $(containers.controls);
+  },
 
-	setPlayerInfo: function(id, color, soldiers, nodes, score)
-	{
-		var row = $(this.playerInfoName.interpolate({id: id}));
+  setPlayerInfo: function (id, color, soldiers, nodes, score) {
+    var row = $(this.playerInfoName.interpolate({
+      id: id
+    }));
 
-		if (!row) { return; }
+    if (!row) {
+      return;
+    }
 
-		row.down('.color').setStyle({"background-color": color});
-		row.down('.soldiers').update(soldiers)
-		row.down('.nodes').update(nodes);
-		row.down('.score').update(score);
-	},
+    row.down('.color').setStyle({
+      "background-color": color
+    });
+    row.down('.soldiers').update(soldiers)
+    row.down('.nodes').update(nodes);
+    row.down('.score').update(score);
+  },
 
-	/*
-	 * Update the progress bar
-	 */
-	updateProgressPercent: function (percent)
-	{
-		this.progressBar.setStyle("width: #{percent}%".interpolate({percent: percent}));
-	},
+  /*
+   * Update the progress bar
+   */
+  updateProgressPercent: function (percent) {
+    this.progressBar.setStyle("width: #{percent}%".interpolate({
+      percent: percent
+    }));
+  },
 
-	/*
-	 * Update the current turn #
-	 */
-	updateProgressTurns: function (turnNumber, maxTurns)
-	{
-		var format = "#{currentTurn}/#{nbTurns}";
+  /*
+   * Update the current turn #
+   */
+  updateProgressTurns: function (turnNumber, maxTurns) {
+    var format = "#{currentTurn}/#{nbTurns}";
 
-		var data =
-		{
-			currentTurn: Math.ceil(turnNumber || 0),
-			nbTurns: Math.ceil(maxTurns || 0)
-		};
+    var data = {
+      currentTurn: Math.ceil(turnNumber || 0),
+      nbTurns: Math.ceil(maxTurns || 0)
+    };
 
-		this.progressTurns.update(format.interpolate(data));
-	},
+    this.progressTurns.update(format.interpolate(data));
+  },
 })
 
 TS.AIPlayback = Class.create(TS.Playback, {
-	initialize: function ($super, containers, map_url, history_url, options)
-	{
-		$super(containers, map_url, history_url);
+  initialize: function ($super, containers, map_url, history_url, options) {
+    $super(containers, map_url, history_url);
 
-		// create the visualizer
-		this.map = new TS.AIMap(containers.map, map_url, options);
-		this.map.observe('ready', this.onMapReady.bindAsEventListener(this));
+    // create the visualizer
+    this.map = new TS.AIMap(containers.map, map_url, options);
+    this.map.observe('ready', this.onMapReady.bindAsEventListener(this));
 
-		// other shit
-		this.turnNumber = 0;
-		this.forward = true;
-		this.ready = {map:false, self:false};
+    // other shit
+    this.turnNumber = 0;
+    this.forward = true;
+    this.ready = {
+      map: false,
+      self: false
+    };
 
-		// create the buttons
-		this.buttons = ["rewind", "back", "play", "pause", "next", "end"];
-		this.buttons.each(function (control) {
-			this[control] = this.controls.down("#" + control);
-			this[control].observe("click", this["on" + control.capitalize()].bindAsEventListener(this));
-		}, this);
+    // create the buttons
+    this.buttons = ["rewind", "back", "play", "pause", "next", "end"];
+    this.buttons.each(function (control) {
+      this[control] = this.controls.down("#" + control);
+      this[control].observe("click", this["on" + control.capitalize()].bindAsEventListener(this));
+    }, this);
 
-		// listen to keyboard keys
-		TS.Keyboard.registerCallback(" ", [], this.togglePlayPause.bind(this));
-		TS.Keyboard.registerCallback(Event.KEY_LEFT, [], this.onBack.bindAsEventListener(this));
-		TS.Keyboard.registerCallback(Event.KEY_RIGHT, [], this.onNext.bindAsEventListener(this));
-		TS.Keyboard.registerCallback(Event.KEY_TAB, [], this.toggleDebug.bindAsEventListener(this));
+    // listen to keyboard keys
+    TS.Keyboard.registerCallback(" ", [], this.togglePlayPause.bind(this));
+    TS.Keyboard.registerCallback(Event.KEY_LEFT, [], this.onBack.bindAsEventListener(this));
+    TS.Keyboard.registerCallback(Event.KEY_RIGHT, [], this.onNext.bindAsEventListener(this));
+    TS.Keyboard.registerCallback(Event.KEY_TAB, [], this.toggleDebug.bindAsEventListener(this));
 
-		// create the timer for automatic playback
-		this.timer = new TS.Timer();
-		this.timer.observe("timer", this.onTimer.bind(this));
+    // create the timer for automatic playback
+    this.timer = new TS.Timer();
+    this.timer.observe("timer", this.onTimer.bind(this));
 
-		this.enableControls();
-		this.playbackDescription = null;
-		this.stepTime = 500;
-	},
+    this.enableControls();
+    this.playbackDescription = null;
+    this.stepTime = 500;
+  },
 
-	/*
-	 * Called when the game description is fetched from the server
-	 */
-	onGameDescriptionLoaded: function (request)
-	{
-		// parse the answer
-		this.gameDescription = (request.responseJSON || JSON.parse(request.responseText)).game.replay;
-		this.ready.self = true;
+  /*
+   * Called when the game description is fetched from the server
+   */
+  onGameDescriptionLoaded: function (request) {
+    // parse the answer
+    this.gameDescription = (request.responseJSON || JSON.parse(request.responseText)).game.replay;
+    this.ready.self = true;
 
-		// draw the first turn (setup)
-		if (this.isReady()) {
-			// create the playback description
-			this.playbackDescription = new TS.PlaybackDescription(
-				this.map.config,
-				this.map.nodeGraph,
-				this.gameDescription,
-				this.map.layers['background'],
-				this.map.graphics,
-				this.stepTime);
-			this.playbackDescription.initializeTurns();
+    // draw the first turn (setup)
+    if (this.isReady()) {
+      // create the playback description
+      this.playbackDescription = new TS.PlaybackDescription(
+        this.map.config,
+        this.map.nodeGraph,
+        this.gameDescription,
+        this.map.layers['background'],
+        this.map.graphics,
+        this.stepTime);
+      this.playbackDescription.initializeTurns();
 
-			this.enableControls();
-			this.drawCurrentTurn();
-		}
-	},
+      this.enableControls();
+      this.drawCurrentTurn();
+    }
+  },
 
-	/*
-	 * Button callback: Rewind
-	 */
-	onRewind: function (e)
-	{
-		this.onPause();
-		this.turnNumber = 0;
-		this.forward = false;
-		this.drawCurrentTurn();
-	},
+  /*
+   * Button callback: Rewind
+   */
+  onRewind: function (e) {
+    this.onPause();
+    this.turnNumber = 0;
+    this.forward = false;
+    this.drawCurrentTurn();
+  },
 
-	/*
-	 * Button callback: Back
-	 */
-	onBack: function (e)
-	{
-		e.stop();
-		this.onPause();
-		if (this.turnNumber > 0)
-		{
-			this.turnNumber--;
-			this.forward = false;
-			this.drawCurrentTurn();
-		}
-	},
+  /*
+   * Button callback: Back
+   */
+  onBack: function (e) {
+    e.stop();
+    this.onPause();
+    if (this.turnNumber > 0) {
+      this.turnNumber--;
+      this.forward = false;
+      this.drawCurrentTurn();
+    }
+  },
 
-	/*
-	 * Button callback: Play
-	 */
-	onPlay: function (e)
-	{
-		if (this.turnNumber >= this.getMaxTurn())
-			this.turnNumber = 0;
+  /*
+   * Button callback: Play
+   */
+  onPlay: function (e) {
+    if (this.turnNumber >= this.getMaxTurn())
+      this.turnNumber = 0;
 
-		this.start();
-		this.enableControls();
-	},
+    this.start();
+    this.enableControls();
+  },
 
-	/*
-	 * Button callback: Pause
-	 */
-	onPause: function ()
-	{
-		this.stop();
-		this.enableControls();
-	},
+  /*
+   * Button callback: Pause
+   */
+  onPause: function () {
+    this.stop();
+    this.enableControls();
+  },
 
-	/*
-	 * Toggle between the Play/Pause buttons
-	 */
-	togglePlayPause: function (e)
-	{
-		e.stop();
-		this.timer.isRunning()? this.onPause() : this.onPlay();
-	},
+  /*
+   * Toggle between the Play/Pause buttons
+   */
+  togglePlayPause: function (e) {
+    e.stop();
+    this.timer.isRunning() ? this.onPause() : this.onPlay();
+  },
 
-	/*
-	 * Button callback: Next
-	 */
-	onNext: function (e)
-	{
-		e.stop();
-		this.onPause();
-		if (this.turnNumber < this.getMaxTurn())
-		{
-			this.turnNumber++;
-			this.forward = true;
-			this.drawCurrentTurn();
-		}
-	},
+  /*
+   * Button callback: Next
+   */
+  onNext: function (e) {
+    e.stop();
+    this.onPause();
+    if (this.turnNumber < this.getMaxTurn()) {
+      this.turnNumber++;
+      this.forward = true;
+      this.drawCurrentTurn();
+    }
+  },
 
-	/*
-	 * Button callback: End
-	 */
-	onEnd: function (e)
-	{
-		this.turnNumber = this.getMaxTurn();
-		this.forward = true;
-		this.drawCurrentTurn();
-	},
+  /*
+   * Button callback: End
+   */
+  onEnd: function (e) {
+    this.turnNumber = this.getMaxTurn();
+    this.forward = true;
+    this.drawCurrentTurn();
+  },
 
-	/*
-	 * Button callback: Toogle debug
-	 */
-	toggleDebug: function (e)
-	{
-		e.stop();
-		this.map.layers['debug'].setActive(!this.map.layers['debug'].active);
-		this.map.layers['debug'].refreshCount = 0;
-		this.map.drawDebugLayer(this.playbackDescription.turns[0]);
-	},
+  /*
+   * Button callback: Toogle debug
+   */
+  toggleDebug: function (e) {
+    e.stop();
+    this.map.layers['debug'].setActive(!this.map.layers['debug'].active);
+    this.map.layers['debug'].refreshCount = 0;
+    this.map.drawDebugLayer(this.playbackDescription.turns[0]);
+  },
 
-	/*
-	 * Draw the current frame of the simulation
-	 */
-	drawCurrentTurn: function ()
-	{
-		// draw the current frame
-		this.map.doTurn(
-			this.playbackDescription.turns[this.turnNumber + (this.forward? -1 : 1)],
-			this.playbackDescription.turns[this.turnNumber],
-			this.forward);
+  /*
+   * Draw the current frame of the simulation
+   */
+  drawCurrentTurn: function () {
+    // draw the current frame
+    this.map.doTurn(
+      this.playbackDescription.turns[this.turnNumber + (this.forward ? -1 : 1)],
+      this.playbackDescription.turns[this.turnNumber],
+      this.forward);
 
-		// update other shit
-		this.enableControls();
-		this.updatePlayerList();
-		this.updateProgress();
-	},
+    // update other shit
+    this.enableControls();
+    this.updatePlayerList();
+    this.updateProgress();
+  },
 
-	updateProgress: function()
-	{
-		this.updateProgressPercent(this.turnNumber/this.getMaxTurn() * 100);
-		var currentTurn = (this.turnNumber / this.playbackDescription.simulatioGameRatio) || 0
-		var nbTurns = (this.getMaxTurn() / this.playbackDescription.simulatioGameRatio) || 0
+  updateProgress: function () {
+    this.updateProgressPercent(this.turnNumber / this.getMaxTurn() * 100);
+    var currentTurn = (this.turnNumber / this.playbackDescription.simulatioGameRatio) || 0
+    var nbTurns = (this.getMaxTurn() / this.playbackDescription.simulatioGameRatio) || 0
 
-		this.updateProgressTurns(currentTurn, nbTurns);
-	},
+    this.updateProgressTurns(currentTurn, nbTurns);
+  },
 
-	/*
-	 * Upgrade the player infos
-	 */
-	updatePlayerList: function ()
-	{
-		Object.values(this.playbackDescription.turns[Math.min(this.turnNumber, this.getMaxTurn() - 1)].players).each(function(player){
-			this.setPlayerInfo(player.id, player.color, player.soldiers, player.nodes, player.score);
-		}, this);
-	},
+  /*
+   * Upgrade the player infos
+   */
+  updatePlayerList: function () {
+    Object.values(this.playbackDescription.turns[Math.min(this.turnNumber, this.getMaxTurn() - 1)].players).each(function (player) {
+      this.setPlayerInfo(player.id, player.color, player.soldiers, player.nodes, player.score);
+    }, this);
+  },
 
-	/*
-	 * Get the number of simulation turns.
-	 * A simulation turn is 1/5 of a game turn.
-	 */
-	getMaxTurn: function ()
-	{
-		return this.playbackDescription.turns.length;
-	},
+  /*
+   * Get the number of simulation turns.
+   * A simulation turn is 1/5 of a game turn.
+   */
+  getMaxTurn: function () {
+    return this.playbackDescription.turns.length;
+  },
 
-	/*
-	 * Called when the visualizer is ready
-	 * i.e. the config was succesfully loaded
-	 */
-	onMapReady: function ()
-	{
-		this.ready.map = true;
+  /*
+   * Called when the visualizer is ready
+   * i.e. the config was succesfully loaded
+   */
+  onMapReady: function () {
+    this.ready.map = true;
 
-		// request the game description
-		new Ajax.Request( this.history_url, {method: 'get', onComplete: this.onGameDescriptionLoaded.bindAsEventListener(this)});
-	},
+    // request the game description
+    new Ajax.Request(this.history_url, {
+      method: 'get',
+      onComplete: this.onGameDescriptionLoaded.bindAsEventListener(this)
+    });
+  },
 
-	/*
-	 * Checks if the playback is ready
-	 * i.e. the visualizer and the playback are ready.
-	 */
-	isReady: function ()
-	{
-		return $H(this.ready).all();
-	},
+  /*
+   * Checks if the playback is ready
+   * i.e. the visualizer and the playback are ready.
+   */
+  isReady: function () {
+    return $H(this.ready).all();
+  },
 
-	/*
-	 * Set the controls depending on the state of the simulation
-	 */
-	enableControls: function ()
-	{
-		this.buttons.each(function(control){
-			this[control].disabled = this.isReady() ? "" : "disabled";
-		},this);
+  /*
+   * Set the controls depending on the state of the simulation
+   */
+  enableControls: function () {
+    this.buttons.each(function (control) {
+      this[control].disabled = this.isReady() ? "" : "disabled";
+    }, this);
 
-		this.play[this.timer.isRunning() ? "hide" : "show"]();
-		this.pause[this.timer.isRunning() ? "show" : "hide"]();
+    this.play[this.timer.isRunning() ? "hide" : "show"]();
+    this.pause[this.timer.isRunning() ? "show" : "hide"]();
 
-		if (this.turnNumber == 0)
-		{
-			this.back.disabled = "disabled";
-			this.rewind.disabled = "disabled";
-		} else if (this.turnNumber >= this.getMaxTurn())
-		{
-			this.next.disabled = "disabled";
-			this.end.disabled = "disabled;"
-		}
-	},
+    if (this.turnNumber == 0) {
+      this.back.disabled = "disabled";
+      this.rewind.disabled = "disabled";
+    } else if (this.turnNumber >= this.getMaxTurn()) {
+      this.next.disabled = "disabled";
+      this.end.disabled = "disabled;"
+    }
+  },
 
-	/*
-	 * Stop the automatic playback
-	 */
-	stop: function ()
-	{
-		this.timer.stop();
-	},
+  /*
+   * Stop the automatic playback
+   */
+  stop: function () {
+    this.timer.stop();
+  },
 
-	/*
-	 * Start the automatic playback
-	 */
-	start: function ()
-	{
-		this.timer.start(this.stepTime, -1);
-	},
+  /*
+   * Start the automatic playback
+   */
+  start: function () {
+    this.timer.start(this.stepTime, -1);
+  },
 
-	/*
-	 * Process one turn in automatic playback
-	 */
-	onTimer: function ()
-	{
-		this.forward = true;
+  /*
+   * Process one turn in automatic playback
+   */
+  onTimer: function () {
+    this.forward = true;
 
-		if (this.timer.isRunning())
-			this.turnNumber++;
+    if (this.timer.isRunning())
+      this.turnNumber++;
 
-		this.drawCurrentTurn();
+    this.drawCurrentTurn();
 
-		if (this.turnNumber >= this.getMaxTurn()) {
-			this.timer.stop();
-			return;
-		}
-	}
+    if (this.turnNumber >= this.getMaxTurn()) {
+      this.timer.stop();
+      return;
+    }
+  }
 });
 
 TS.FlashPlayback = Class.create(TS.Playback, {
-	initialize: function($super, containers, map_url, history_url)
-	{
-		$super(containers, map_url, history_url);
-		this.setupFlash();
-	},
+  initialize: function ($super, containers, map_url, history_url) {
+    $super(containers, map_url, history_url);
+    this.setupFlash();
+  },
 
-	setupFlash: function()
-	{
-		// For version detection, set to min. required Flash Player version, or 0 (or 0.0.0), for no version detection.
+  setupFlash: function () {
+    // For version detection, set to min. required Flash Player version, or 0 (or 0.0.0), for no version detection.
     var swfVersionStr = "10.2.0";
     // To use express install, set to playerProductInstall.swf, otherwise the empty string.
-    var flashvars = {map: this.map_url, history: this.history_url};
+    var flashvars = {
+      map: this.map_url,
+      history: this.history_url
+    };
     var params = {};
     params.quality = "high";
     params.bgcolor = "#202020";
@@ -361,9 +339,9 @@ TS.FlashPlayback = Class.create(TS.Playback, {
     attributes.name = "berlin-client";
     attributes.align = "middle";
     swfobject.embedSWF(
-        "/berlin-client.swf", this.containers.flash,
-        "960", "500",
-        swfVersionStr, null,
-        flashvars, params, attributes);
-	},
+      "/berlin-client.swf", this.containers.flash,
+      "960", "500",
+      swfVersionStr, null,
+      flashvars, params, attributes);
+  },
 })
--- simulator.js
+++ simulator.js
@@ -1,159 +1,167 @@
-/**
- * author: Guillaume Malette <gmalette@gmail.com> @gmalette
- * author: Christian Blais <christ.blais@gmail.com> @christianblais
- * author: Jodi Giordano <giordano.jodi@gmail.com> @jodi
- * website: thirdside.ca
- * date: 07/03/2011
- */
-
-
 TS.AIMap = Class.create(TS, {
-	initialize: function ($super, container, config_url, options)
-	{
-		$super();
+  initialize: function ($super, container, config_url, options) {
+    $super();
 
-		this.options = Object.extend({
-			engine: "SVG"
-		}, options || {});
+    this.options = Object.extend({
+      engine: "SVG"
+    }, options || {});
 
-		this.config_url = config_url;
-		this.container  = $(container);
-		this.layers     = {};
-		this.graphics   = {};
-		this.color      = new TS.Color();
-		this.size       = {width: this.container.getWidth(), height: this.container.getHeight()};
+    this.config_url = config_url;
+    this.container = $(container);
+    this.layers = {};
+    this.graphics = {};
+    this.color = new TS.Color();
+    this.size = {
+      width: this.container.getWidth(),
+      height: this.container.getHeight()
+    };
 
-		// load config
-		new Ajax.Request( config_url, {method: 'get', onComplete: this.onConfigLoaded.bindAsEventListener(this)});
-	},
+    // load config
+    new Ajax.Request(config_url, {
+      method: 'get',
+      onComplete: this.onConfigLoaded.bindAsEventListener(this)
+    });
+  },
 
-	/*
-	 * Callback after the visualizer's config is loaded
-	 */
-	onConfigLoaded: function (request)
-	{
+  /*
+   * Callback after the visualizer's config is loaded
+   */
+  onConfigLoaded: function (request) {
     this.config = (request.responseJSON || JSON.parse(request.responseText)).map.representation;
-		this.imagesToLoad = this.config.images.size();
+    this.imagesToLoad = this.config.images.size();
 
-		if (!this.config) {alert("Map Error"); return};
+    if (!this.config) {
+      alert("Map Error");
+      return
+    };
 
-		this.nodeGraph = new TS.NodeGraph(this.config);
+    this.nodeGraph = new TS.NodeGraph(this.config);
 
-		// Create a canvas element for each display layer
-		$A(['background', 'paths', 'nodes', 'moves', 'soldiers', 'combats', 'spawns', 'debug']).each(function(layer) {
-			this.layers[layer] = new TS[this.options.engine](
-				this.container,
-				{
-					position: { x: 0, y: 0 },
-					size: this.size,
-			    active: ['debug'].indexOf(layer) <= -1,
-			    mustRefresh: ['nodes', 'moves', 'soldiers', 'combats', 'spawns'].indexOf(layer) > -1,
-				  translate: this.config.infos.translate
-				}
-			);
-		}, this);
+    // Create a canvas element for each display layer
+    $A(['background', 'paths', 'nodes', 'moves', 'soldiers', 'combats', 'spawns', 'debug']).each(function (layer) {
+      this.layers[layer] = new TS[this.options.engine](
+        this.container, {
+          position: {
+            x: 0,
+            y: 0
+          },
+          size: this.size,
+          active: ['debug'].indexOf(layer) <= -1,
+          mustRefresh: ['nodes', 'moves', 'soldiers', 'combats', 'spawns'].indexOf(layer) > -1,
+          translate: this.config.infos.translate
+        }
+      );
+    }, this);
 
 
-		// Preload all the images
-		this.config.images.each(function(data) {
-			var img = new Image();
-			img.onload = this.onImageLoaded.bindAsEventListener(this, data.name);
-			img.src = (this.options.local ? "." : "") + data.src;
-		}, this);
-	},
+    // Preload all the images
+    this.config.images.each(function (data) {
+      var img = new Image();
+      img.onload = this.onImageLoaded.bindAsEventListener(this, data.name);
+      img.src = (this.options.local ? "." : "") + data.src;
+    }, this);
+  },
 
-	/*
-	 * Callback after each image is loaded
-	 */
-	onImageLoaded: function (event, name)
-	{
-		this.graphics[name] = event.findElement();
+  /*
+   * Callback after each image is loaded
+   */
+  onImageLoaded: function (event, name) {
+    this.graphics[name] = event.findElement();
 
-		this.imagesToLoad--;
+    this.imagesToLoad--;
 
-		// when all the images are loaded, tell the world.
-		if (this.imagesToLoad == 0)
-			this.fire("ready");
-	},
+    // when all the images are loaded, tell the world.
+    if (this.imagesToLoad == 0)
+      this.fire("ready");
+  },
 
-	/*
-	 * Called every time a new turn happens
-	 */
-	doTurn: function (turnBefore, turnNow, forward)
-	{
-		// if a turnBefore exists, we must play the departure animations
-		if (turnBefore)
-		{
-			Object.keys(turnBefore.layers).each(function(layerName) {
-				var layer = this.layers[layerName];
+  /*
+   * Called every time a new turn happens
+   */
+  doTurn: function (turnBefore, turnNow, forward) {
+    // if a turnBefore exists, we must play the departure animations
+    if (turnBefore) {
+      Object.keys(turnBefore.layers).each(function (layerName) {
+        var layer = this.layers[layerName];
 
-				if (!layer.getMustRefresh()) { return; }
+        if (!layer.getMustRefresh()) {
+          return;
+        }
 
-				// clear the layer
-				layer.clear();
+        // clear the layer
+        layer.clear();
 
-				if (!layer.active) { return; }
+        if (!layer.active) {
+          return;
+        }
 
-				var data = turnBefore.layers[layerName];
+        var data = turnBefore.layers[layerName];
 
-				// add the objects
-				data.objects.each(function (object) {
-					layer.addObject(
-						object,
-						data[forward ? 'forward_departure' : 'backward_departure'][object.id].start);
-				}, this);
+        // add the objects
+        data.objects.each(function (object) {
+          layer.addObject(
+            object,
+            data[forward ? 'forward_departure' : 'backward_departure'][object.id].start);
+        }, this);
 
-				// add the animations
-				Object.keys(data[forward ? 'forward_departure' : 'backward_departure']).each(function (animationId) {
-					var animation = data[forward ? 'forward_departure' : 'backward_departure'][animationId];
-					layer.addAnimation(animationId, animation.end, animation.length);
-				}, this);
-			}, this);
-		}
+        // add the animations
+        Object.keys(data[forward ? 'forward_departure' : 'backward_departure']).each(function (animationId) {
+          var animation = data[forward ? 'forward_departure' : 'backward_departure'][animationId];
+          layer.addAnimation(animationId, animation.end, animation.length);
+        }, this);
+      }, this);
+    }
 
-		if (turnNow) {
-			Object.keys(turnNow.layers).each(function(layerName){
-				var layer = this.layers[layerName];
+    if (turnNow) {
+      Object.keys(turnNow.layers).each(function (layerName) {
+        var layer = this.layers[layerName];
 
-				if (!layer.getMustRefresh()) { return; }
+        if (!layer.getMustRefresh()) {
+          return;
+        }
 
-				// clear the layer
-				layer.clear();
+        // clear the layer
+        layer.clear();
 
-				if (!layer.active) { return; }
+        if (!layer.active) {
+          return;
+        }
 
-				var data = turnNow.layers[layerName];
+        var data = turnNow.layers[layerName];
 
-				// add the objects
-				data.objects.each(function(object){
-					layer.addObject(object, data[forward ? 'forward_arrival' : 'backward_arrival'][object.id].start);
-				}, this);
+        // add the objects
+        data.objects.each(function (object) {
+          layer.addObject(object, data[forward ? 'forward_arrival' : 'backward_arrival'][object.id].start);
+        }, this);
 
-				// add the animations
-				Object.keys(data[forward ? 'forward_arrival' : 'backward_arrival']).each(function(animationId){
-					var animation = data[forward ? 'forward_arrival' : 'backward_arrival'][animationId];
-					layer.addAnimation(animationId, animation.end, animation.length);
-				}, this);
-			}, this);
-		}
-	},
+        // add the animations
+        Object.keys(data[forward ? 'forward_arrival' : 'backward_arrival']).each(function (animationId) {
+          var animation = data[forward ? 'forward_arrival' : 'backward_arrival'][animationId];
+          layer.addAnimation(animationId, animation.end, animation.length);
+        }, this);
+      }, this);
+    }
+  },
 
-	drawDebugLayer: function (turn)
-	{
-		var layer = this.layers['debug'];
+  drawDebugLayer: function (turn) {
+    var layer = this.layers['debug'];
 
-		if (!layer.getMustRefresh()) { return; }
+    if (!layer.getMustRefresh()) {
+      return;
+    }
 
-		// clear the layer
-		layer.clear();
+    // clear the layer
+    layer.clear();
 
-		if (!layer.active) { return; }
+    if (!layer.active) {
+      return;
+    }
 
-		var data = turn.layers['debug'];
+    var data = turn.layers['debug'];
 
-		// add the objects
-		data.objects.each(function(object){
-			layer.addObject(object, data['forward_arrival'][object.id].start);
-		}, this);
-	}
+    // add the objects
+    data.objects.each(function (object) {
+      layer.addObject(object, data['forward_arrival'][object.id].start);
+    }, this);
+  }
 });
